f5be9a715fe0916047626b0400efb63176479acd
ChihChiu29 <chih.chiu.19@gmail.com>
2017-08-20 22:49:48 -0700

    Updated examples to try out struct and inheritance.

---
+++ src/examples/geometry.go
@@ -0,0 +1,24 @@
+package examples
+
+import "fmt"
+
+type IntPair struct {
+	X, Y int
+}
+
+func (intPair IntPair) Draw() {
+	fmt.Println("Draw at location:", intPair)
+}
+
+type Point struct {
+	IntPair
+	Color int
+}
+
+func (p *Point) ChangeColor(color int) {
+	p.Color = color
+}
+
+func (p Point) Draw() {
+	fmt.Println("Draw at point", p.IntPair, "with color", p.Color)
+}
