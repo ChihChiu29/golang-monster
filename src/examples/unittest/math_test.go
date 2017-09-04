10cfd522101583735ef78975406a57c3dd1e0f5f
ChihChiu29 <chih.chiu.19@gmail.com>
2017-09-03 20:52:14 -0700

    add an example to the unit test

--- src/examples/unittest/math_test.go
+++ src/examples/unittest/math_test.go
@@ -1,9 +1,23 @@
 package unittest
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 	if Add(3, 4) != 7 {
 		t.Error("no!!")
 	}
+}
+
+func BenchmarkAdd(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Add(3, 4)
+	}
+}
+
+func ExampleAdd() {
+	fmt.Println(Add(3, 4))
+	// Output: 7
 }
